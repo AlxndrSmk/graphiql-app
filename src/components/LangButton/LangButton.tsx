23d7c71aa25d9320745e8c3c38eb5f8ef9438f2d
BrBrov <epoc@list.ru>
2023-12-07 22:59:13 +0300

    refactor: change styles lang button

--- src/components/LangButton/LangButton.tsx
+++ src/components/LangButton/LangButton.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useContext, useEffect, useState } from 'react';
+import {ReactNode, useContext, useEffect, useReducer, useState} from 'react';
 import LangContext from '@/types/LangContext';
 import LanguageContext from '@/context/langContext';
 import { useRouter } from 'next/router';
@@ -10,18 +10,21 @@ function LangButton(): ReactNode {
   const router = useRouter();
   const lang: string | null = checkQueryParams(router);
   const [isClicked, setIsClicked] = useState(false);
+  const [btnStyle, setBtnStyle]
+    = useReducer( onAction, `${styles.language_button} ${styles.en_bg}`);
 
   useEffect(() => {
     if (!lang) {
       router.replace('main?lang=en').then(() => context.setPageLang('en'));
     } else {
       context.setPageLang(lang);
+      setBtnStyle({type: lang});
     }
   }, [lang]);
 
   return (
     <div className={styles.language_wrapper}>
-      <button className={styles.language_button} onClick={onChangeLang}>
+      <button className={btnStyle} onClick={onChangeLang}>
         <span className={styles.language_label}>
           {context.getConstants().langButton}
         </span>
@@ -35,6 +38,17 @@ function LangButton(): ReactNode {
       context.pageLang = context.pageLang === 'en' ? 'ru' : 'en';
       await router.push(`./main?lang=${context.pageLang}`);
       setIsClicked(false);
+    }
+  }
+
+  function onAction(_state: string, action: {type: string}): string {
+    switch (action.type) {
+      case 'en':
+        return `${styles.language_button} ${styles.en_bg}`;
+      case 'ru':
+        return `${styles.language_button} ${styles.ru_bg}`;
+      default:
+        return `${styles.language_button} ${styles.en_bg}`;
     }
   }
 }
