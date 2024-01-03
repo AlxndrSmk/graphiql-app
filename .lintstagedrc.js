230c60ad27a7f2d2366610437f76ebb6aa049aff
BrBrov <borbrov@gmail.com>
2024-01-03 23:50:41 +0300

    fix: fix lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,10 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 module.exports = {
-  '**/*.ts?(x)': ['npm run lint:fix src', 'npm run prettier-fix'],
+  '**/*.ts?(x)': [buildEslintCommand, 'npm run prettier-fix'],
 };
