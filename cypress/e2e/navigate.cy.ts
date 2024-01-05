aafb83c299bdb1210266ba4d2914c22d58259a4b
Alexander Samak <voprosy262@gmail.com>
2024-01-05 21:46:41 +0300

    add tests for signIn, navigation and msToTime util function

---
+++ cypress/e2e/navigate.cy.ts
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Navigate to sigin page', () => {
+    cy.get('[data-testid="signin-button"]').click();
+    cy.url().should('include', '/signin');
+  });
+
+  it('Navigate to sigup page', () => {
+    cy.get('[data-testid="signup-button"]').click();
+    cy.url().should('include', '/signup');
+  });
+});
+
+export {};
